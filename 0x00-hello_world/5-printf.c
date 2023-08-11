d6c5fa7b0a4947c4b1b9a54d431bc423c912a477
eiman680 <eyzo30.ali30@gmail.com>
2023-08-10 17:31:37 -0700

    er

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Return: always 0 (success)
+ */
+int main(void)
+{
+	printf(*with proper grammar, but the outcome is a piece of art,\n*)i
+		return (0)i
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+include <stdio.h>
+/**
+ *main - Entry point
+ *Return: always 0 (success)
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)", sizeof(char))i
+	printf("Size of an int: %d byte(s)", sizeof(int))i
+	printf("Size of a long int: %d byte(s)", sizeof(long int))i
+	printf("Size of a long long int: %d byte(s)", sizeof(long long int))i
+	printf("Size of a float: %d byte(s)", sizeof(float))i
+	return (0)i
+}
